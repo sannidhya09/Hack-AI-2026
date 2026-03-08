a813d157d801080fdb2874b1b037ef25343653b6
Sannidhya Tiwari <sannidhya.tiwari09@gmail.com>
2026-03-08 03:07:02 -0500

    fix port from environment

--- backend/main.py
+++ backend/main.py
@@ -410,4 +410,5 @@ async def music_search(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
